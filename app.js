a266edee108cd750900110301c5bca99f92ecd9e
Yashwanth sai <20r91a0530@tkrec.ac.in>
2022-04-23 04:40:10 +0530

    First Heroku commit

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req,res){
+
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields:{
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us14.api.mailchimp.com/3.0/lists/e841d176b7"
+
+    const options = {
+      method: "POST",
+      auth: "yashwanth5:a747b625213b8c3a49476e92b27bced5-us14"
+    }
+
+    const request = https.request(url, options,function(response){
+
+        if(response.statusCode === 200){
+          res.sendFile(__dirname+"/success.html");
+        }
+        else{
+          res.sendFile(__dirname+"/failure.html");
+        }
+
+
+      response.on("data",function(data){
+        console.log(JSON.parse(data));
+
+    })
+  });
+
+    // request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure", function(req,res){
+  res.redirect("/");
+})
+
+
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is hosted in 3000!");
+});
+//
+//
+
+
+
+//api key
+// a747b625213b8c3a49476e92b27bced5-us14
+
+// list id
+// e841d176b7
